ceebeb75bf757e571a4ffef467d6d977876d61f2
unbearableDarkness <29276823+unbearableDarkness@users.noreply.github.com>
2021-05-03 18:46:15 +0200

    Update image_downloader.py

--- image_downloader.py
+++ image_downloader.py
@@ -20,10 +20,11 @@ THREADS_NUM = 16
 
 
 class Grabber:
-    def __init__(self, dest_path, thread_url):
+    def __init__(self, dest_path, board, thread_id):
         self.destination_path = dest_path
-        self.tread_url = thread_url
         self.jobs = deque()
+        self.board = board
+        self.thread_id = thread_id
         self.producer_running = True
         self.t_threads = []
 
@@ -34,9 +35,7 @@ class Grabber:
 
     def download(self):
         try:
-            board = self.tread_url.split('/')[3]
-            thread_id = self.tread_url.split('/')[5]
-            thread_endpoint = "https://a.4cdn.org/{0}/thread/{1}.json".format(board, thread_id)
+            thread_endpoint = "https://a.4cdn.org/{0}/thread/{1}.json".format(self.board, self.thread_id)
             thread_response = requests.get(thread_endpoint)
             thread_data = json.loads(thread_response.text)
         except (rex.HTTPError, rex.InvalidURL) as e:
@@ -44,21 +43,18 @@ class Grabber:
             print("Could not fetch {1}. {0}\n".format(thread_endpoint, e), end='')
             sys.exit(-1)
 
-		cc = 0
         # noinspection PyUnboundLocalVariable
         for post in thread_data["posts"]:
             # timestamp
             if "tim" in post:
-				cc += 1
                 # noinspection PyUnboundLocalVariable
-                img_url = "https://i.4cdn.org/{0}/{1}{2}".format(board, post["tim"], post["ext"])
+                img_url = "https://i.4cdn.org/{0}/{1}{2}".format(self.board, post["tim"], post["ext"])
                 img_name = "{0}{1}".format(post["tim"], post["ext"])
                 img_destination_path = os.path.join(destination_path, img_name)
                 self.jobs.append((img_url, img_destination_path))
         self.producer_running = False
         for t in self.t_threads:
             t.join()
-		return cc
 
     def consumer(self):
         while True:
@@ -75,24 +71,21 @@ class Grabber:
 if __name__ == '__main__':
     myurl = input("Enter the thread URL: ")
 
-    # parsers = argparse.ArgumentParser()
-    # parsers.add_argument("id", type=str)
-    # args = parsers.parse_args()
-
-    # https://boards.4channel.org/[board]/thread/[#thread]
+    # https://boards.4channel.org/[board]/threadID[[#bottom|#p[postID] --optional]
+    # https://boards.4channel.org/g/12345#bottom
+    # https://boards.4channel.org/g/12345#p12345
 
     try:
         if myurl.split('/')[3] not in boards:
             print("Error, that board doesn't exist.")
-            exit(-1)
-
+            sys.exit(-1)
     except IndexError as ex:
-        print(ex, "\n", end='')
-        print("Error, please enter a valid url\n.", end='')
+        # print(ex, "\n", end='')
+        print("Error, please enter a valid url.\n", end='')
         sys.exit(-1)
 
     myboard:str = myurl.split('/')[3]
-    mythread_id:str = myurl.split('/')[5]
+    mythread_id:str = (myurl.split('/')[5]).partition('#')[0]
     directory:str = "{0}-{1}".format(myboard, mythread_id)
     destination_folder:str = os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), IMAGES_ROOT_FOLDER)
     destination_path:str = os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), IMAGES_ROOT_FOLDER, directory)
@@ -105,11 +98,12 @@ if __name__ == '__main__':
         os.mkdir(destination_path)
     except (FileExistsError, PermissionError) as ex:
         print(ex, '\n', end='')
-		sys.exit(-1)
+        sys.exit(-1)
 
     t1 = time.time()
-    g = Grabber(destination_path, myurl)
-    cc = g.download()
+    g = Grabber(destination_path, myboard, mythread_id)
+    g.download()
+    nfiles = len([name for name in os.listdir(destination_path)])
     t2 = time.time()
-    print("Time taken: {0} seconds. Downloaded {1} images.".format(round(t2 - t1)), cc)
+    print("Time taken: {0} seconds. Downloaded {1} images/videos.".format(round(t2 - t1), nfiles))
     sys.exit(0)
